7bd573b9f1f55bf751220b0d33976bc498f7cbef
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-30 09:03:43 +0530

    finish Course.FastAnagrams

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -7,6 +7,7 @@ import Course.Core
 import Course.List
 import Course.Functor
 import qualified Data.Set as S
+import qualified Data.List as L
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
@@ -14,8 +15,8 @@ fastAnagrams ::
   Chars
   -> Filename
   -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams s f = filter ((== k) . NoCaseString . listh . L.sort . hlist) . lines <$> readFile f
+                   where k = NoCaseString $ listh $ L.sort (hlist s)
 
 newtype NoCaseString =
   NoCaseString {
