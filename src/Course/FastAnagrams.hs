f875bca074b171dcfe6c0c733b20cd3b99bc8092
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-30 09:08:00 +0530

    include tonymorris' implementation for comparison

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -18,6 +18,24 @@ fastAnagrams ::
 fastAnagrams s f = filter ((== k) . NoCaseString . listh . L.sort . hlist) . lines <$> readFile f
                    where k = NoCaseString $ listh $ L.sort (hlist s)
 
+
+-- tonymorris' implementation, for comparison
+--
+--   λ> fastAnagrams "leaf" "/usr/share/dict/words"
+--   ["alef","feal","flea","leaf"]
+--   (3.22 secs, 2,134,406,464 bytes)
+--   λ> tfastAnagrams "leaf" "/usr/share/dict/words"
+--   ["leaf","alef","feal","flea"]
+--   (8.20 secs, 1,342,759,448 bytes)
+
+tfastAnagrams ::
+  Chars
+  -> Filename
+  -> IO (List Chars)
+tfastAnagrams name f =
+  (flip (filter . flip S.member) (permutations name) . S.fromList . hlist . lines) <$> readFile f
+
+
 newtype NoCaseString =
   NoCaseString {
     ncString :: Chars
